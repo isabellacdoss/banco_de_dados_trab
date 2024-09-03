a889cdae75b81a1415de6c8c18164023d685ec5c
isabellacdoss <isabella.c.santos@edu.ufes.br>
2024-09-03 09:45:14 -0300

    Update Isabella.sql

--- Isabella.sql
+++ Isabella.sql
@@ -1,3 +1,26 @@
-select 1
-"blablabla
-oi isa, tudo bem"/
+
+-- criando o bando de dados 
+-- CREATE DATABASE previdencia;
+USE previdencia;
+
+-- criando a tabela endereço
+CREATE TABLE endereco
+(codEND INT AUTO_INCREMENT PRIMARY KEY, 
+cidade VARCHAR(20), 
+CEP CHAR(8), 
+logradouro VARCHAR (20),
+estado VARCHAR (20));
+
+-- criando a tabela cliente
+CREATE TABLE cliente
+(CPF CHAR(11), 
+nome VARCHAR(50),
+RG CHAR(11),
+FOREIGN KEY (CNPJ) REFERENCES banco(CNPJ), 
+FOREIGN KEY (codEND) REFERENCES endereco(codEND)); 
+
+
+
+
+
+
