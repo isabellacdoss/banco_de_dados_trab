9368472c6e9b702378a065b03d2be99895b52152
isabellacdoss <isabella.c.santos@edu.ufes.br>
2024-09-04 15:58:20 -0300

    Update Isabella.sql

--- Isabella.sql
+++ Isabella.sql
@@ -1,43 +1,29 @@
+use previdencia; 
 
--- criando o bando de dados 
--- CREATE DATABASE previdencia;
-USE previdencia;
+-- Criando tabela para receber cpf
+CREATE TABLE dados1 (
+   cpf VARCHAR(11));
 
--- criando a tabela endereço
-CREATE TABLE endereco
-(codEND INT AUTO_INCREMENT PRIMARY KEY, 
-cidade VARCHAR(20), 
-CEP CHAR(8), 
-logradouro VARCHAR (20),
-estado VARCHAR (20));
+CREATE TABLE dados2 (
+   cnpj VARCHAR(14));
 
--- criando a tabela cliente 
-CREATE TABLE cliente
-(CPF CHAR(11), 
-nome VARCHAR(50),
-RG CHAR(11),
-FOREIGN KEY (CNPJ) REFERENCES banco(CNPJ),  -- a tabela banco é da mika
-FOREIGN KEY (codEND) REFERENCES endereco(codEND)); 
+INSERT INTO dados1 VALUES ('11111111111'), ('08207241730');
 
+INSERT INTO dados2 VALUES ('00101010101999'), ('11111117307771');
 
+-- Criação da tabela de junção das duas colunas
+CREATE TABLE juncao
+(cpf_cnpj VARCHAR(14));
 
--- criando a tabela telefone
-CREATE TABLE telefone
-(DD_numero CHAR(2), 
-numero CHAR(9))
- -- cpnj ou cpf como chave primária e secundária: como colocar?
- 
--- criando a tabela funcionário
--- CREATE  TABLE funcionario
--- (CPF CHAR(11) PRIMARY KEY, FOREIGN KEY (CPF) REFERENCES cliente (CPF),
--- FOREIGN KEY (codEND) REFERENCES endereco(codEND));
+-- Inserindo os dados na tabela juncao
+INSERT INTO juncao (cpf_cnpj)
+SELECT cpf 
+FROM dados1 as cpf_cnpj
+UNION 
+SELECT cnpj 
+FROM dados2;
 
--- criando a tabela empresa
-CREATE TABLE empresa
-(CNPJ CHAR(14) PRIMARY KEY,
-nome VARCHAR(30), 
-FOREIGN KEY (codEND) REFERENCES endereco(codEND));
-
+select * from juncao;
 
 
 
