94bf92535a00168d97713961c5763b83a32c4057
isabellacdoss <isabella.c.santos@edu.ufes.br>
2024-09-05 10:08:32 -0300

    Update codigo para juntar cpf e cnpj.sql

--- codigo para juntar cpf e cnpj.sql
+++ codigo para juntar cpf e cnpj.sql
@@ -11,7 +11,8 @@ insert into clientes values ('12345678910');
 insert into empresas values ('12345678910234');
 insert into associacao  values ('12345678910234');
 
-CREATE TABLE juncao_cpf_cnpj (cpf_cnpj	varchar (14))
+-- criando uma tabela que junta cpf e cnpj
+CREATE TABLE juncao_cpf_cnpj (cpf_cnpj	varchar (14), cod int auto_increment primary key)
 SELECT cpf as cpf_cnpj
 FROM clientes
 UNION ALL
@@ -20,3 +21,6 @@ union all
 select cnpj from associacao;
 
 select * from juncao_cpf_cnpj;
+
+-- relacionando com endereco
+
