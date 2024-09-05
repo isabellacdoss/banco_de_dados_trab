59cfc205ade5ebf603f06c58c5d50dc6ce4e2d95
Alicelage <153660514+Alicelage@users.noreply.github.com>
2024-09-05 11:37:11 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -93,19 +93,12 @@ ALTER TABLE previdenciaP
 ADD CONSTRAINT FK_previdenciaP_banco
 FOREIGN KEY (banco_CNPJ) REFERENCES banco(CNPJ);
 
--- Alterando a tabela previdenciaP e adicionando a coluna contribuicao
+-- Alterando a tabela funcionario e previdenciaP e adicionando a coluna contribuicao
 ALTER TABLE funcionario
 ADD COLUMN contribuicao DECIMAL(5,3) NOT NULL;
 
-UPDATE funcionario
-SET contribuicao = 0.075 where salario < 2826.65;
-UPDATE funcionario
-SET contribuicao = 0.15 where 2826.66 < salario < 3751.05;
-UPDATE funcionario
-SET contribuicao = 0.225 where 3751.06 < salario < 4664.68;
-UPDATE funcionario
-SET  contribuicao = 0.275 where salario > 4664.69;
-
+alter table previdenciaP
+add column contribuicao decimal(5,3) not null;
 
 -- Criando tabela taxas:
 CREATE TABLE taxas(
